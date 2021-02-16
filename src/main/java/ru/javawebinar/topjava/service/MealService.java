d67407377f689f204bf5acc988c2428bab11ce5d
Rob Lagranhe <lagranherob@gmail.com>
2021-02-16 22:30:00 +0100

    Add CRUD methods to MealService

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -2,7 +2,12 @@ package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 @Service
 public class MealService {
@@ -14,5 +19,23 @@ public class MealService {
         this.repository = repository;
     }
 
+    public Meal create(int userId, Meal meal){
+        return repository.save(userId, meal);
+    }
 
+    public void delete(int userId, int id){
+        checkNotFoundWithId(repository.delete(userId, id), id);
+    }
+
+    public Meal get(int userId, int id){
+        return checkNotFoundWithId(repository.get(userId, id), id);
+    }
+
+    public Collection<Meal> getAll(int userId){
+        return repository.getAll(userId);
+    }
+
+    public void update(int userId, Meal meal){
+        checkNotFoundWithId(repository.save(userId, meal), meal.getId());
+    }
 }
